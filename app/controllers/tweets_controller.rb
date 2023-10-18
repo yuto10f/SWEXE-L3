a1dfb361774e0f646fb5cad0d65b0b1b75bf02d5
Ubuntu <ubuntu@ip-172-31-41-248.ec2.internal>
2023-10-18 02:01:13 +0000

    push to render.com

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,8 @@
 class TweetsController < ApplicationController
     def index
       @tweets = Tweet.all
+      logger.debug("******")
+      
     end
     def new
       @tweet = Tweet.new
@@ -9,7 +11,7 @@ class TweetsController < ApplicationController
       @tweet = Tweet.new(message: params[:tweet][:message], tdate: Time.current)
       if @tweet.save
         flash[:notice] = '1レコード追加しました'
-        redirect_to '/'
+        redirect_to root_path
       else
         render 'new'
       end
@@ -18,7 +20,7 @@ class TweetsController < ApplicationController
       tweet = Tweet.find(params[:id])
       tweet.destroy
       flash[:notice] = '1レコード削除しました'
-      redirect_to '/'
+      redirect_to root_path
     end
     def show
       @tweet = Tweet.find(params[:id])
@@ -30,7 +32,7 @@ class TweetsController < ApplicationController
       @tweet = Tweet.find(params[:id])
       if @tweet.update(message: params[:tweet][:message])
         flash[:notice] = '1レコード更新しました'
-        redirect_to '/'
+        redirect_to root_path
       else
         render 'edit'
       end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "6a10b58fced54474a4857e8fbae305bc.vfs.cloud9.us-east-1.amazonaws.com"
 end
